f297784cfb5e4ae8089a6b62e0c0874e1cbe167a
ewilkinson896 <erin.wilkinson@Kainos.com>
2025-09-23 10:34:25 +0100

    Refactored to typescript - errors

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response): void => {
+  res.send('This is a change');
+});
+
+app.listen(port, (): void => {
+  console.log(`Example app listening on https://localhost:${port}`);
+});
