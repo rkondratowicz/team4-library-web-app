465178c72929c838d7097ffdde8374be8d2d53fb
ewilkinson896 <erin.wilkinson@Kainos.com>
2025-09-23 10:45:13 +0100

    Refactored to ts
    Issue where changes arent being shown

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { type Request, type Response } from 'express';
+
+const app = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Hello World!');
+});
+
+app.listen(port, (): void => {
+  console.log(`Example app listening on https://localhost:${port}`);
+});
