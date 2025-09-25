bf9467d94468ae1ede8869fd9c2794497e5cdfa9
WesleySloan21 <wesley.sloan@kainos.com>
2025-09-25 16:46:05 +0100

    added analytics

--- src/container/Container.ts
+++ src/container/Container.ts
@@ -1,9 +1,12 @@
 // Dependencies
 import { SQLiteDatabaseRepository, SQLiteBookRepository } from '../repositories/index.js';
+import { SQLiteAnalyticsRepository } from '../repositories/SQLiteAnalyticsRepository.js';
 import { BookService } from '../services/BookService.js';
 import { DatabaseService } from '../services/DatabaseService.js';
+import { AnalyticsService } from '../services/AnalyticsService.js';
 import { BookController } from '../controllers/BookController.js';
 import { MainController } from '../controllers/MainController.js';
+import { AnalyticsController } from '../controllers/AnalyticsController.js';
 
 /**
  * Simple Dependency Injection Container
@@ -25,21 +28,27 @@ export class Container {
     
     // Register other repositories
     const bookRepo = new SQLiteBookRepository(databaseRepo.getDatabase());
+    const analyticsRepo = new SQLiteAnalyticsRepository(databaseRepo.getDatabase());
     this.registerSingleton('bookRepository', bookRepo);
+    this.registerSingleton('analyticsRepository', analyticsRepo);
     
     // Register services
     const bookService = new BookService(bookRepo);
     const databaseService = new DatabaseService(databaseRepo);
+    const analyticsService = new AnalyticsService(analyticsRepo);
     
     this.registerSingleton('bookService', bookService);
     this.registerSingleton('databaseService', databaseService);
+    this.registerSingleton('analyticsService', analyticsService);
     
     // Register controllers
     const bookController = new BookController(bookService);
     const mainController = new MainController(bookService, databaseService);
+    const analyticsController = new AnalyticsController(analyticsService, bookService);
     
     this.registerSingleton('bookController', bookController);
     this.registerSingleton('mainController', mainController);
+    this.registerSingleton('analyticsController', analyticsController);
   }
 
   /**
@@ -72,6 +81,13 @@ export class Container {
    */
   getMainController(): MainController {
     return this.get<MainController>('mainController');
+  }
+
+  /**
+   * Get the analytics controller
+   */
+  getAnalyticsController(): AnalyticsController {
+    return this.get<AnalyticsController>('analyticsController');
   }
 
   /**
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 // Route exports
 export { createBookRoutes } from './bookRoutes.js';
 export { createMainRoutes } from './mainRoutes.js';
+export { createAnalyticsRoutes, createAnalyticsApiRoutes } from './analyticsRoutes.js';
